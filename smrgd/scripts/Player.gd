6a1368eee326c216485060f7754baa7220a70f6a
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2024-10-01 18:11:59 -0300

    run & jump animations

--- smrgd/scripts/Player.gd
+++ smrgd/scripts/Player.gd
@@ -1,9 +1,14 @@
 extends CharacterBody2D
 
-const SPEED = 180
+
+const SPEED = 170
 const JUMP_VELOCITY = -450
 const GRAVITY = 1800
 @onready var animated_sprite_2d = $AnimatedSprite2D
+
+
+func _process(delta):
+	UpdateAnimation()
 
 
 func _physics_process(delta):
@@ -20,3 +25,17 @@ func _physics_process(delta):
 		velocity.x = 0
 		
 	move_and_slide()
+
+
+func UpdateAnimation():
+	if velocity.x != 0:
+		animated_sprite_2d.flip_h = velocity.x < 0
+		
+	if is_on_floor():
+		if abs(velocity.x) >= 0.1:
+			animated_sprite_2d.play("Run")
+		else:
+			animated_sprite_2d.play("Idle")
+	else:
+		animated_sprite_2d.play("Jump")		
+			

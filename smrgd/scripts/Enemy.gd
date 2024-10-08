9ebbe5bb196bb4964f264542ed6dff1f211efc88
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2024-10-08 15:11:36 -0300

    enemy raycasts for edges and obstacles

--- smrgd/scripts/Enemy.gd
+++ smrgd/scripts/Enemy.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 const SPEED = 50
 var direction = -1
 @onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var ray_cast_2d_forward: RayCast2D = $CollisionShape2D/RayCast2D_Forward
+@onready var ray_cast_2d_downward: RayCast2D = $CollisionShape2D/RayCast2D_Downward
 
 
 func _process(delta):
@@ -11,7 +13,11 @@ func _process(delta):
 func _physics_process(delta):
 	if is_on_floor() == false:
 		velocity.y = 300
-		
+	
+	if ray_cast_2d_forward.is_colliding() || ray_cast_2d_downward.is_colliding() == false:
+		direction = -direction
+		ray_cast_2d_forward.target_position.x = -ray_cast_2d_forward.target_position.x
+		ray_cast_2d_downward.target_position.x = -ray_cast_2d_downward.target_position.x
 	velocity.x = direction * SPEED
 	
 	move_and_slide()

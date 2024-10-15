3667cf6e943f1293c99a62ad76c112bf5204ecab
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2024-10-14 22:21:51 -0300

    enemy health system

--- smrgd/scripts/Enemy.gd
+++ smrgd/scripts/Enemy.gd
@@ -1,29 +1,52 @@
 extends CharacterBody2D
+class_name EnemyController
 
 const SPEED = 50
 var direction = -1
 @onready var animated_sprite_2d = $AnimatedSprite2D
-@onready var ray_cast_2d_forward: RayCast2D = $CollisionShape2D/RayCast2D_Forward
-@onready var ray_cast_2d_downward: RayCast2D = $CollisionShape2D/RayCast2D_Downward
+@onready var ray_cast_2d_forward = $CollisionShape2D/RayCast2D_Forward
+@onready var ray_cast_2d_downward = $CollisionShape2D/RayCast2D_Downward
 
+var currentHealth = 100
+var isDead = false
 
-func _process(delta):
+func _process(_delta):
 	UpdateAnimation()
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	if is_on_floor() == false:
 		velocity.y = 300
+	
+	if isDead:
+		return
 	
 	if ray_cast_2d_forward.is_colliding() || ray_cast_2d_downward.is_colliding() == false:
 		direction = -direction
 		ray_cast_2d_forward.target_position.x = -ray_cast_2d_forward.target_position.x
-		ray_cast_2d_downward.target_position.x = -ray_cast_2d_downward.target_position.x
+		ray_cast_2d_downward.position.x = -ray_cast_2d_downward.position.x
+		
 	velocity.x = direction * SPEED
 	
 	move_and_slide()
 	
 func UpdateAnimation():
+	if isDead:
+		return
+		
 	if velocity.x != 0:
 		animated_sprite_2d.flip_h = velocity.x > 0
 		
 	animated_sprite_2d.play("Walk")
+
+func ApplyDamage(damage : int):
+	if isDead:
+		return
+		
+	currentHealth -= damage
+	
+	if currentHealth <= 0:
+		isDead = true
+		animated_sprite_2d.play("Die")
+		set_collision_layer_value(3,false)
+		await get_tree().create_timer(2).timeout
+		queue_free()
--- smrgd/scripts/bullet.gd
+++ smrgd/scripts/bullet.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
-@onready var bullet_sprite_2d: Sprite2D = $Bullet_Sprite2D
+
+@onready var bullet_sprite_2d = $Bullet_Sprite2D
 
 const SPEED = 500
 var direction = 1
@@ -11,15 +12,21 @@ func _physics_process(delta):
 		bullet_sprite_2d.flip_h = true
 		
 	position.x += SPEED * direction * delta
-
-
-func _on_body_entered(body: Node2D) -> void:
-	print("The bullet hit!")
 	
+	
+	
+
+
+func _on_body_entered(body):
+
 	var vfxToSpawn = preload("res://Scenes/vfx_bullet_hit.tscn")
-	var vfxInstance = GameManager.SpawnVFX(vfxToSpawn, global_position)
+	var vfxInstance = GameManager.SpawnVFX(vfxToSpawn,global_position)
 	
 	if direction == -1:
 		vfxInstance.scale.x = -1
-		
+	
+	var enemy = body as EnemyController
+	if enemy:
+		enemy.ApplyDamage(DAMAGE)
+	
 	queue_free()
